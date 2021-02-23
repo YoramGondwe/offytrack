c9211105f22218677a2a3d39eded2ddd7bff2483
yoram-g <yoramgondwe3rd@gmail.com>
2021-02-23 16:35:16 +0200

    setup some database structures

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4264789bcd230486cee6a54c10039ef9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ce93dd0-b7d2-43c1-a6eb-950df8cbb931'
+};
